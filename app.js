411eff268a3b2bf57c7a326cd4b6bfe3b4919414
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-17 15:58:12 +0100

    feat: refactoring iniziato

--- app.js
+++ app.js
@@ -17,37 +17,7 @@ app.get("/", (req, res) => {
     res.send("IL MIO SERVER")
 })
 
-//Array dei post (guarda non si capiva....)
-const posts = [
-    {
-        id: "1",
-        title: "Post-1",
-        content: "Oggi ho creato il mio primo server, in locale e senza l'aiuto di matteoStrap !!",
-        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
 
-    },
-    {
-        id: "2",
-        title: "Post-2",
-        content: "se solo avessi avuto BootStrap... senza di lui mi sento perso :(",
-        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
-
-    },
-    {
-        id: "3",
-        title: "Post-3",
-        content: "Da domani npm install Vado a Correre, P.S: non ci credi nemmeno tu !",
-        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
-
-    },
-    {
-        id: "4",
-        title: "Post-4",
-        content: "Miao Corporation: azieda esclusiva per chi usa solo BootStrap e non matteoStrap. Nata il giorno: 404 Error (esiste da sempre : D)",
-        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david", "node ./miao-main.Loris"]
-        
-    },
-]
 
 
 app.get("/bacheca", (req, res) => {
---
+++ routers/posts.js
@@ -0,0 +1,18 @@
+import posts from "../data/data.js";
+
+import express from "express";
+
+const rounter = express.Router();
+
+
+//INDEX 
+
+//SHOW 
+
+//STORE
+
+//UPDATE
+
+//MODIFY
+
+//DESTROY 
