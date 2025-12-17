320153c777ba9a697c7fedfebe1275bfec751b1a
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-17 16:47:23 +0100

    feat: CRUD completati : D

--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 import posts from "../data/data.js";
 
-import express from "express";
+import express, { Router } from "express";
 
 const rounter = express.Router();
 
@@ -21,12 +21,27 @@ rounter.get("/:id", (req, res) => {
     res.json(post)
 })
 //STORE
+rounter.post("/", (req, res) => {
+    res.send("creato nuovo post")
+})
+
 
 //UPDATE
+rounter.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send("modificato il post con id:" + id)
+})
 
 //MODIFY
-
+rounter.patch("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send("modica parziale del post con id:" + id)
+})
 //DESTROY 
+rounter.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send("hai eliminato il post con id:" + id)
+})
 
 
 
