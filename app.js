7344f53e170580421ea2e0c689873361569363b9
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-17 15:51:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log("Hello Boolean !!");
+
+import { log } from "console";
+import express from "express"
+import { title } from "process";
+
+//express senza sicilia (questo mi fa male...)
+const app = express();
+const port = 3000;
+
+//per rendere la cartella public publica (mano sui capelli...)
+//url per le immagini: localhost:3000/images/ciambellone.jpeg ----> restituisce l'immagine !!!!!!!
+app.use(express.static("public"));
+
+//API base che restituisce la scritta: "IL MIO SERVER"
+app.get("/", (req, res) => {
+    res.send("IL MIO SERVER")
+})
+
+//Array dei post (guarda non si capiva....)
+const posts = [
+    {
+        id: "1",
+        title: "Post-1",
+        content: "Oggi ho creato il mio primo server, in locale e senza l'aiuto di matteoStrap !!",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "2",
+        title: "Post-2",
+        content: "se solo avessi avuto BootStrap... senza di lui mi sento perso :(",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "3",
+        title: "Post-3",
+        content: "Da domani npm install Vado a Correre, P.S: non ci credi nemmeno tu !",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "4",
+        title: "Post-4",
+        content: "Miao Corporation: azieda esclusiva per chi usa solo BootStrap e non matteoStrap. Nata il giorno: 404 Error (esiste da sempre : D)",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david", "node ./miao-main.Loris"]
+        
+    },
+]
+
+
+app.get("/bacheca", (req, res) => {
+   const tag = req.query.tag
+   console.log(tag);
+   
+   res.json(posts)
+})
+
+
+
+//ascolto del server (ma c'è davvero bisogno di spiegare tutto ? )
+app.listen(port, () => {
+    console.log(`Server avviato con successo nella porta ${port}`);
+
+})
+
